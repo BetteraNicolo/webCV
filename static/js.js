e4b4fddddb0c73fed2c77a7cc68725cd30f9cf6a
Bettera Nicolò <betteranicolo@gmail.com>
2024-07-31 16:50:24 +0200

    Add files via upload

--- static/js.js
+++ static/js.js
@@ -97,8 +97,32 @@ function traduciPortfolio(lang) {
         document.getElementById("JSText").textContent   =   "This course introduces the fundamental concepts of programming using JavaScript. It covers the basics of the language's syntax, control structures, functions, objects, and interaction with the DOM (Document Object Model) for web content manipulation.";
         document.getElementById("CCNAText").textContent =   "This is the first module of the CCNA (Cisco Certified Network Associate) program, introducing the basic concepts of networking. It covers topics such as network architecture, communication models, IP addressing, protocols, and essential network services."
         document.getElementById("cert").textContent     =   "Cisco certifications";
+        document.getElementById("superiori").textContent=   "Obtained during the final year of high school";
         document.getElementById('cv-link').href         =   "pdf/BetteraNicolo-CVENG.pdf";
+        document.getElementById("itExp").textContent    =   "IT Experience";
+        document.getElementById("lista1").textContent   =   "Modification and updating of company websites using HTML, PHP, CSS, JavaScript, and SQL.";
+        document.getElementById("lista2").textContent   =   "Troubleshooting hardware and software issues on PCs and laptops, including replacement of components such as memory, processors, motherboards, and power supplies.";
+        document.getElementById("lista3").textContent   =   "Cloning and restoring physical drives.";
+        document.getElementById("lista4").textContent   =   "Creation of graphic content using Photoshop and Illustrator.";
+        document.getElementById("lista5").textContent   =   "Formatting and configuring devices on Windows and macOS.";
+        document.getElementById("lista6").textContent   =   "Password recovery and cybersecurity management.";
+        document.getElementById("lista7").textContent   =   "Installation of operating systems and software.";
+        document.getElementById("lista8").textContent   =   "Technical support and remote assistance for devices and printers.";
+        document.getElementById("stgC").textContent     =   "Curricular internship";
+        document.getElementById("posizione").textContent=   "Position: IT Technician";
+        document.getElementById("periodo").innerHTML    =   "<strong>Duration:</strong> June 2023 - July 2023";
+        document.getElementById("descS").innerHTML      =   "<strong>Description:</strong> This project was undertaken as an extracurricular activity, with the goal of having fun and learning web programming. We created an application that allows the class to play together in two games, with the teacher acting as the administrator. Here’s what it offers:";
+        document.getElementById("azienda").innerHTML    =   "<strong>Company:</strong> <u><a href='https://www.clickitsolutions.it' target='_blank'> Click it Solutions </a></u>  ";
+        document.getElementById("prjS").textContent     = "School Project";
+        document.getElementById("prjM").innerHTML       = 'Project: <u><a href="https://www.sitinosetosobellino.altervista.org/progettoMemory/" target="_blank">Memory</a></u>';
+        document.getElementById("descP").innerHTML      = "<strong>Description:</strong> This project was undertaken as an extracurricular activity, with the goal of having fun and learning web programming. We created an application that allows the class to play together in two games, with the teacher acting as the administrator. Here’s what it offers:";
+        document.getElementById("memory").innerHTML     = "<strong>Memory Mode:</strong> Students must match between 8 and 32 cards, depending on the device's space (for example, the school interactive whiteboard showed all 32 cards). Half of the cards show historical figures, while the other half contains the name and a brief description. This game helps to memorize historical figures while having fun.";
+        document.getElementById("indovina").innerHTML   = "<strong>Guess Who Mode:</strong> Users participate in the same session. The teacher can manage the characters, time, hints, and check scores. During the game, the name of the character is gradually revealed to help participants. At the end, results and scores are shown on the screen.";
+        document.getElementById("host").textContent     = "The project is now available online for everyone. The teacher can, with the provided credentials, update and modify the characters, allowing for future expansions and improvements to the game.";
+        document.getElementById("tecno").innerHTML      = "<strong>Technologies:</strong> JavaScript, SQL, HTML, CSS, PHP";
 
+        
+        
     }
     else{
         document.getElementById("perito1").textContent  =   "PERITO INFORMATICO";
@@ -107,7 +131,32 @@ function traduciPortfolio(lang) {
         document.getElementById("JSText").textContent   =   "Questo corso introduce i concetti fondamentali di programmazione utilizzando JavaScript. Copre le basi della sintassi del linguaggio, le strutture di controllo, le funzioni, gli oggetti e l'interazione con il DOM (Document Object Model) per la manipolazione dei contenuti web.";
         document.getElementById("CCNAText").textContent =   "Questo è il primo modulo del programma CCNA (Cisco Certified Network Associate) che introduce i concetti di base delle reti. Copre argomenti come l'architettura delle reti, i modelli di comunicazione, gli indirizzi IP, i protocolli e i servizi fondamentali per la comunicazione su rete.";
         document.getElementById("cert").textContent     =   "Certificazioni Cisco";
+        document.getElementById("superiori").textContent=   "Ottenute durante l'ultimo anno di Superiori";
         document.getElementById('cv-link').href         =   "pdf/BetteraNicolo-CVITA.pdf";
+        document.getElementById("itExp").textContent    =   "Esperienze IT";
+        document.getElementById("lista1").textContent   =   "Modifica e aggiornamento di siti web aziendali utilizzando HTML, PHP, CSS, JavaScript e SQL.";
+        document.getElementById("lista2").textContent   =   "Risoluzione di problemi hardware e software su PC e laptop, incluse sostituzioni di componenti come memorie, processori, schede madri e alimentatori.";
+        document.getElementById("lista3").textContent   =   "Clonazione e ripristino di memorie fisiche.";
+        document.getElementById("lista4").textContent   =   "Creazione di contenuti grafici con Photoshop e Illustrator.";
+        document.getElementById("lista5").textContent   =   "Formattazione e configurazione di dispositivi su Windows e MacOS.";
+        document.getElementById("lista6").textContent   =   "Recupero password e gestione della sicurezza informatica.";
+        document.getElementById("lista7").textContent   =   "Installazione di sistemi operativi e software.";
+        document.getElementById("lista8").textContent   =   "Assistenza tecnica e teleassistenza per dispositivi e stampanti.";
+        document.getElementById("stgC").textContent     =   "Stage curricolare";
+        document.getElementById("posizione").textContent=   "Posizione: Tecnico IT";
+        document.getElementById("periodo").innerHTML    =   "<strong>Durata:</strong> Giugno 2023 - Luglio 2023";
+        document.getElementById("descS").innerHTML      =   "<strong>Descrizione:</strong> Questo progetto è stato realizzato come attività extra, con l'obiettivo di divertirci e imparare la programmazione web. Abbiamo creato un'applicazione che permette alla classe di giocare insieme a due giochi, con l'insegnante che funge da amministratore. Ecco cosa offre:";
+        document.getElementById("azienda").innerHTML    =   "<strong>Azienda:</strong> <u><a href='https://www.clickitsolutions.it' target='_blank'> Click it Solutions </a></u>  ";
+        document.getElementById("prjS").textContent     =   "Progetto Scolastico";
+        document.getElementById("prjM").innerHTML       =   'Progetto: <u><a href="https://www.sitinosetosobellino.altervista.org/progettoMemory/" target="_blank">Memory</a></u>';
+        document.getElementById("descP").innerHTML      =   "<strong>Descrizione:</strong> Questo progetto è stato realizzato come attività extra, con l'obiettivo di divertirci e imparare la programmazione web. Abbiamo creato un'applicazione che permette alla classe di giocare insieme a due giochi, con l'insegnante che funge da amministratore. Ecco cosa offre:";
+        document.getElementById("memory").innerHTML     =   "<strong>Modalità Memory:</strong> Gli studenti devono abbinare tra 8 e 32 figurine, a seconda dello spazio del dispositivo (ad esempio, la LIM scolastica mostrava tutte e 32 le figurine). Metà delle figurine mostrano personaggi storici, mentre l'altra metà contiene il nome e una breve descrizione. Questo gioco aiuta a memorizzare i personaggi storici divertendosi.";
+        document.getElementById("indovina").innerHTML   =   "<strong>Modalità Indovina Chi:</strong> Gli utenti partecipano alla stessa sessione. L'insegnante può gestire i personaggi, il tempo, i suggerimenti e controllare i punteggi. Durante il gioco, il nome del personaggio viene svelato gradualmente per aiutare i partecipanti. Alla fine, i risultati e i punteggi vengono mostrati sullo schermo.";
+        document.getElementById("host").textContent     =   "Il progetto è ora disponibile online per tutti. L'insegnante può, con le credenziali fornite, aggiornare e modificare i personaggi, permettendo così future espansioni e miglioramenti del gioco.";
+        document.getElementById("tecno").innerHTML      =   "<strong>Tecnologie:</strong> JavaScript, SQL, HTML, CSS, PHP";
+         
+ 
+
     }       
 
 
